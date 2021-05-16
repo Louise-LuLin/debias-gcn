91bf4e8d6941266144b8c9bdab85e9e69b19ce00
Louise-LuLin <lulin199209@gmail.com>
2021-05-16 18:59:10 -0400

    evaluation of embeddings

--- eval.py
+++ eval.py
@@ -19,6 +19,18 @@ from create_dataset import MyDataset
 def sigmoid(z):
     return 1/(1 + np.exp(-z))
 
+def dcg_at_k(r, k):
+    r = np.asfarray(r)[:k]
+    if r.size != k:
+        raise ValueError('Ranking List length < k')    
+    return np.sum((2**r - 1) / np.log2(np.arange(2, r.size + 2)))
+
+def ndcg_at_k(r, k):
+    sort_r = sorted(r,reverse = True)
+    idcg = dcg_at_k(sort_r, k)
+    if not idcg:
+        return 0.
+    return dcg_at_k(r, k) / idcg
 
 dir = './embeddings'
 dataset = 'movielens'
@@ -42,7 +54,13 @@ u, v, eids = graph.edges(form='all')
 scores = np.dot(embeddings, embeddings.T)
 scores = softmax(scores, axis=1)
 
-adj = sp.coo_matrix((np.ones(len(u)), (u.numpy(), v.numpy()))).todense()
+#random
+rd_embeddings = np.random.rand(embeddings.shape[0], embeddings.shape[1])
+rd_scores = np.dot(rd_embeddings, rd_embeddings.T)
+rd_scores = softmax(rd_scores, axis=1)
+
+adj = np.array(sp.coo_matrix((np.ones(len(u)), (u.numpy(), v.numpy()))).todense())
+print (scores.shape, adj.shape, type(scores), type(adj))
 
 # edges grouped by node
 src_nodes = set(u.numpy().tolist()) # all source node idx
@@ -56,28 +74,37 @@ for i in range(len(u.numpy().tolist())):
     eid_dict[(u.numpy()[i], v.numpy()[i])] = eids.numpy()[i]
 
 auc = []
+auc_rd = []
 ndcg = []
+ndcg_rd = []
+myndcg = []
+myndcg_rd = []
 # only treat items as valid candidate set
 mask = np.array([adj.shape[0] + i for i in range(adj.shape[1]-adj.shape[0])])
 i = 0
 for src_n, des_ns in tqdm.tqdm(edge_dict.items()):
     y_pred = scores[src_n, mask]
-    y_pred_flip = 1.0 - y_pred
-
+    y_pred2 = rd_scores[src_n, mask]
     y_true = adj[src_n, mask]
-    y_true_flip = 1 - y_true
 
-    y_pred = np.stack([y_pred, y_pred_flip]).T
-    y_true = np.stack([y_true, y_true_flip]).T
+    # calc auc
+    auc.append( roc_auc_score(y_true, y_pred) )
+    auc_rd.append( roc_auc_score(y_true, y_pred2) )
 
-    auc.append(roc_auc_score(y_true, y_pred))
-    ndcg.append(ndcg_score(y_true, y_pred, k=10))
+    # calc Nan's ndcg
+    myndcg.append( ndcg_at_k(y_pred, 10) )
+    myndcg_rd.append( ndcg_at_k(y_pred2, 10) )
 
-    i+=1
-    if i%50 == 0:
-        print (i, np.mean(np.array(auc)), np.mean(np.array(ndcg)))
+    # calc sklearn ndcg
+    ndcg.append( ndcg_score(np.expand_dims(y_true, axis=0), np.expand_dims(y_pred, axis=0), k=10) )
+    ndcg_rd.append( ndcg_score(np.expand_dims(y_true, axis=0), np.expand_dims(y_pred2, axis=0), k=10) )
 
-auc = np.mean(np.array(auc))
-ndcg = np.mean(np.array(ndcg))
+    i += 1
+    if i % 50 == 0:
+        print ('  model  | auc  | myndcg | sklean ndcg ')
+        print ('  sage   | {:.4f} | {:.4f} | {:.4f}'.format(np.mean(auc), np.mean(myndcg), np.mean(ndcg)))
+        print ('  random | {:.4f} | {:.4f} | {:.4f} '.format(np.mean(auc_rd), np.mean(myndcg_rd), np.mean(ndcg_rd)))
 
-print (auc, ndcg)
+print ('  model  | auc  | myndcg | sklean ndcg ')
+print ('  sage   | {:.4f} | {:.4f} | {:.4f}'.format(np.mean(auc), np.mean(myndcg), np.mean(ndcg)))
+print ('  random | {:.4f} | {:.4f} | {:.4f} '.format(np.mean(auc_rd), np.mean(myndcg_rd), np.mean(ndcg_rd)))
